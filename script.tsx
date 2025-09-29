e99bbcfc47a02869036daadde609789a432b40f6
aleksandra <matealeksandra@gmail.com>
2025-09-29 19:29:45 +0200

    Add second .then() to handle and log parsed JSON data

--- script.tsx
+++ script.tsx
@@ -1,5 +1,8 @@
 const authorContainer = document.getElementById('author-container');
 const loadMoreBtn = document.getElementById('load-more-btn');
 
-fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
+fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
     .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+    })
