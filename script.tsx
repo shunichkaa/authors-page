e9590a60c8fa94bd4f663a5721951b88173d9c7d
aleksandra <matealeksandra@gmail.com>
2025-09-29 19:45:21 +0200

    Destructure author, image, url, bio and include index in forEach callback

--- script.tsx
+++ script.tsx
@@ -15,5 +15,7 @@ fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
     });
 
 const displayAuthors = (authors) => {
-    authors.forEach();
+    authors.forEach(({ author, image, url, bio }, index) => {
+    });
+
 };
