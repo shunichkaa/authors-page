771e4fa943c95c7cad7cbcb971990e7444d9d506
aleksandra <matealeksandra@gmail.com>
2025-09-30 14:50:42 +0200

    bug fixing

--- script.tsx
+++ script.tsx
@@ -1,35 +1,45 @@
-const authorContainer = document.getElementById('author-container');
-const loadMoreBtn = document.getElementById('load-more-btn');
+type Author = {
+  author: string;
+  image: string;
+  url: string;
+  bio: string;
+};
+
+const authorContainer = document.querySelector<HTMLElement>('#author-container');
+const loadMoreBtn = document.querySelector<HTMLButtonElement>('#load-more-btn');
 
 let startingIndex = 0;
 let endingIndex = 8;
-let authorDataArr = [];
+let authorDataArr: Author[] = [];
 
 fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
-    .then((res) => res.json())
-    .then((data) => {
-        authorDataArr = data;
-        displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
-    })
-    .catch((err) => {
-        authorContainer.innerHTML = '<p class="error-msg">There was an error loading the authors</p>';
-    });
+  .then((res) => res.json())
+  .then((data: Author[]) => {
+    authorDataArr = data;
+    displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+  })
+  .catch(() => {
+    if (authorContainer) {
+      authorContainer.innerHTML = '<p class="error-msg">There was an error loading the authors</p>';
+    }
+  });
 
 const fetchMoreAuthors = () => {
-    startingIndex += 8;
-    endingIndex += 8;
+  startingIndex += 8;
+  endingIndex += 8;
 
-    displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
-    if (authorDataArr.length <= endingIndex) {
-        loadMoreBtn.disabled = true;
-        loadMoreBtn.style.cursor = "not-allowed";
-        loadMoreBtn.textContent = 'No more data to load';
-    }
+  displayAuthors(authorDataArr.slice(startingIndex, endingIndex));
+  if (loadMoreBtn && authorDataArr.length <= endingIndex) {
+    loadMoreBtn.disabled = true;
+    loadMoreBtn.style.cursor = 'not-allowed';
+    loadMoreBtn.textContent = 'No more data to load';
+  }
 };
 
-const displayAuthors = (authors) => {
-    authors.forEach(({ author, image, url, bio }, index) => {
-        authorContainer.innerHTML += `
+const displayAuthors = (authors: Author[]) => {
+  if (!authorContainer) return;
+  authors.forEach(({ author, image, url, bio }, index) => {
+    authorContainer.innerHTML += `
     <div id="${index}" class="user-card">
       <h2 class="author-name">${author}</h2>
       <img class="user-img" src="${image}" alt="${author} avatar">
@@ -38,7 +48,9 @@ const displayAuthors = (authors) => {
       <a class="author-link" href="${url}" target="_blank">${author} author page</a>
     </div>
   `;
-    });
+  });
 };
 
-loadMoreBtn.addEventListener('click', fetchMoreAuthors);
+if (loadMoreBtn) {
+  loadMoreBtn.addEventListener('click', fetchMoreAuthors);
+}
