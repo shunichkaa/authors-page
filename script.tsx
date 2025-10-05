82d9f314583bd9dab9d5ee9262aef6505c75d01f
aleksandra <matealeksandra@gmail.com>
2025-10-05 11:48:37 +0200

    Change post title to anchor linking to forum post with target="_blank"

--- script.tsx
+++ script.tsx
@@ -116,15 +116,20 @@ const showLatestPosts = (data) => {
         return `
     <tr>
       <td>
-        <p class="post-title">${title}</p>
+<a 
+  class="post-title" 
+  href="${forumTopicUrl}${slug}/${id}" 
+  target="_blank"
+>
+  ${title}
+</a>
+
         ${forumCategory(category_id)}
       </td>
       <td>
-<td>
-  <div class="avatar-container">
-    ${avatars(posters, users)}
-  </div>
-</td>
+        <div class="avatar-container">
+          ${avatars(posters, users)}
+        </div>
       </td>
       <td>${posts_count - 1}</td>
       <td>${viewCount(views)}</td>
