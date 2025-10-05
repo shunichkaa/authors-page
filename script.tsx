120f1d5738a6abf10d499cf401a3d297f2a7fb12
aleksandra <matealeksandra@gmail.com>
2025-10-05 11:38:31 +0200

    Add if statement to check if user exists

--- script.tsx
+++ script.tsx
@@ -73,6 +73,9 @@ const viewCount = (views) => {
 const avatars = (posters, users) => {
     return posters.map((poster) => {
         const user = users.find((user) => user.id === poster.user_id);
+        if (user) {
+
+        }
     });
 };
 
